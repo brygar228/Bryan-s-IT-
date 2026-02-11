a2668e7666daeddf37598223ee9fa487790a0663
BryanGR <109471755+brygar228@users.noreply.github.com>
2026-02-11 09:52:41 -0600

    Update Join_Domain2.ps1
    mark blanks to fill in

--- Join_Domain2.ps1
+++ Join_Domain2.ps1
@@ -1,9 +1,10 @@
-﻿$domain = "nam.zeomega.loc"
-$username = "adm.brodriguez"
+﻿$domain = "----------"
+$username = "---------"
 
 $password = Read-Host -Prompt "Enter Password for $username" -AsSecureString
 
 $credential = New-Object System.Management.Automation.PSCredential ("$doamin\$username", $password)
 
 Write-Host "Joining $domain..." -ForegroundColor Cyan
+
 Add-Computer -DomainName $domain -Credential $credential -Restart -Force
